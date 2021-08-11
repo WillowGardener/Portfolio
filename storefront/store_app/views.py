88ff8fe5b6d844a07694a8c9073f25294e8e78c0
Griffin Wimminger <impendingdelusion@gmail.com>
2021-08-11 15:05:14 -0700

    basic list function achieved

---
+++ storefront/store_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import *
+
+def shop(request):
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    
+    context = {
+        'products': products,
+        'categories': categories
+    }
+
+    return render(request,'shop.html', context)
